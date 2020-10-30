95ccde9b704f454bcde829ab81b7697fab73e2b9
Tushar Manohar <55486496+TusharManohar@users.noreply.github.com>
2020-10-30 15:25:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+                        Main m=new Main();
+                        m.bank();
+                        System.out.println("Thank you!");
+	}
+ 
+}
