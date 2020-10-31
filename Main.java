6b58999ae947695318c3ef94b79bbbad1db5ada4
Tushar Manohar <55486496+TusharManohar@users.noreply.github.com>
2020-10-31 12:20:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ import java.io.*;
 public class Main
 {
 	public static void main(String[] args) {
-                        Main m=new Main();
-                        m.bank();
                         System.out.println("Thank you!");
 	}
  
